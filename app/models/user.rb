dc331259c0a5d1309069bda1b47567a16475f6d3
Will Drexler <wdrexler@mojolingo.com>
2014-01-01 12:21:42 -0500

    Update find_or_create_from_auth_hash to accept github user hashes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,24 +12,34 @@ class User < ActiveRecord::Base
     true
   end
 
-  def self.find_or_create_from_auth_hash(auth_hash)
+  def self.find_or_create_from_auth_hash(type, auth_hash)
     user_info = auth_hash.info
     user = self.where(uid: auth_hash.uid).first
     unless user
-      user = new(
-        first_name: user_info.first_name,
-        last_name: user_info.last_name,
-        email: user_info.email,
-        phone_number: user_info.phone_number,
-        provider: auth_hash.provider,
-        uid: auth_hash.uid
-      )
-      user.save!
+      case type
+      when :att
+        user = new(
+          first_name: user_info.first_name,
+          last_name: user_info.last_name,
+          email: user_info.email,
+          phone_number: user_info.phone_number,
+          provider: auth_hash.provider,
+          uid: auth_hash.uid
+        )
+        user.save!
+      when :github
+        user = new(
+          name: user_info.name,
+          email: user_info.email,
+          uid: auth_hash.uid
+        )
+        user.save!
+      end
     end
     user
   end
 
   def full_name
-    return "#{self.first_name} #{self.last_name}"
+    return self.name || "#{self.first_name} #{self.last_name}"
   end
 end
