7cc81f68954b4b7bf3c2013ded4d8904cae5b01b
Will Drexler <william.drexler@vonagebusiness.com>
2014-10-02 16:45:12 -0400

    Fix the dev environment
    Update chef cookbook to use new dependency management style
    Update cookbooks to latest
    Update Vagrantfile to use new RVM data format

--- treadmill/metadata.rb
+++ treadmill/metadata.rb
@@ -4,6 +4,12 @@ maintainer_email  "ops@mojolingo.com"
 license           "proprietary"
 description       "Sets up treadmill dev environment"
 recipe            "database", "Sets up a database for Treadmill dev"
+depends           "openssl"
+depends           "postgresql"
+depends           "database"
+depends           "rvm"
+depends           "redis"
+depends           "sudo"
 
 %w{ ubuntu debian }.each do |os|
   supports os
--- treadmill/recipes/database.rb
+++ treadmill/recipes/database.rb
@@ -1,4 +1,5 @@
 ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = "en_US.UTF-8"
+include_recipe "openssl"
 include_recipe "postgresql::server"
 include_recipe "database::postgresql"
 
