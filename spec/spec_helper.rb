f6e956096cb053e506e0cfabdf5ec15b2a67fb61
Ben Klang <bklang@mojolingo.com>
2014-10-18 19:39:29 -0400

    Fix some specs that rely on a cookie token

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+ENV['COOKIE_SECRET'] = 'ABCD' * 15
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
