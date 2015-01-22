18f90fd7a0d8151d066eda729b2d7cb33640cae4
Ben Langfeld <ben@langfeld.me>
2015-01-22 12:04:49 -0200

    Patch around https://github.com/fnichol/chef-rvm/issues/278

--- treadmill/recipes/app.rb
+++ treadmill/recipes/app.rb
@@ -1,5 +1,20 @@
 include_recipe "sudo"
 include_recipe "sipp"
+
+rvm_user = 'vagrant'
+
+gnupg_dir       = "/home/#{rvm_user}/.gnupg"
+gnupg_dir_user  = "chown -R #{rvm_user}:#{rvm_user} #{gnupg_dir};"
+gnupg_dir_root  = "if [ -d #{gnupg_dir} ]; then chown -R root:root #{gnupg_dir}; fi;"
+gnupg_cmd       = "`which gpg2 || which gpg` --keyserver hkp://keys.gnupg.net --recv-keys #{node['rvm']['gpg_key']};"
+
+execute "Adding gpg key to #{rvm_user}" do
+  environment ({"HOME" => "/home/#{rvm_user}"})
+  command "#{gnupg_dir_root} #{gnupg_cmd} #{gnupg_dir_user}"
+  not_if { node['rvm']['gpg_key'].empty? }
+  returns 0
+end
+
 include_recipe "rvm::user"
 include_recipe "redis::server"
 include_recipe "postgresql::client"
