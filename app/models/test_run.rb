ed86d46f812dcf464ab5798b573be7099e94f534
Ben Langfeld <ben@langfeld.me>
2015-02-02 20:21:12 -0200

    Allow specifying “From” user, SIP advertise address and arbitrary SIPp options in the Test Run

--- app/models/test_run.rb
+++ app/models/test_run.rb
@@ -5,7 +5,7 @@ class TestRun < ActiveRecord::Base
   attr_accessible :target, :target_id
   attr_accessible :receiver_scenario, :receiver_scenario_id
   attr_accessible :registration_scenario, :registration_scenario_id
-  attr_accessible :to_user
+  attr_accessible :to_user, :from_user, :advertise_address, :sipp_options
   belongs_to :profile
   belongs_to :scenario
   belongs_to :receiver_scenario, class_name: "Scenario"
