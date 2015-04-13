561c7950192a15540f943dffb7edc7843d982e4c
Ben Langfeld <ben@langfeld.me>
2015-04-13 17:11:48 -0300

    This test doesn't need fixture data

--- spec/requests/scenarios_spec.rb
+++ spec/requests/scenarios_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe "Scenarios" do
   describe "GET /test_runs" do
-    let(:test_run_id) { 1 }
-    before do
-      FactoryGirl.create(:scenario, id: test_run_id)
-    end
-
     it "has the placeholder text" do
       login_user
       get new_scenario_path
