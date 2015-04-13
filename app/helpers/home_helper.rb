6876f45460199c1cab6d95c9d18a928aaeefbb84
Ben Langfeld <ben@langfeld.me>
2015-04-13 17:31:59 -0300

    Clean up a messy implementation\

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,11 @@
 module HomeHelper
   def get_completed_test_runs
-    graph_data = [{key: "Completed Successfully", values: []}]
-    complete = TestRun.accessible_by(current_ability).where(state: "complete").group("date(completed_at)").count :id
-    complete.each do |k,v|
-      graph_data[0][:values] << [k, v]
-    end
-    graph_data.to_json
+    complete = TestRun.accessible_by(current_ability).where(state: "complete")
+    [
+      {
+        key: "Completed Successfully",
+        values: complete.group("date(completed_at)").count(:id).to_a
+      }
+    ].to_json
   end
 end
